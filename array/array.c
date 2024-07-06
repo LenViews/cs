b2623e295d58e07c6a1bb5d04fd45d52487a97cf
lenox <lenoxviews@gmail.com>
2024-07-06 19:44:51 +0900

    Display all elements

--- array/array.c
+++ array/array.c
@@ -3,6 +3,9 @@ int main ()
 {
     int num[] = {4, 6, 3, 12, 99, 20};
     //modify fifth element
-    num[6] = 1;
-    printf("%d", num[6]); 
+    num[4] = 1;
+    for(int i=0; i < sizeof(num) / sizeof(num[0]); i++)
+    {
+        printf("%d", num[i]);
+    }
 }
--- array/array.java
+++ array/array.java
@@ -1,6 +1,9 @@
 public class Main{
     public static void main (String [] args){
         String[] cars = {"Tesla", "Bentley", "Rolls Royce", "Range Rover"};
-        System.out.println(cars[1]);
+        // System.out.println(cars[1]);
+        for(int i = 0; i < cars.length; i++){
+            System.out.println(cars[i]);
+        }
     }
 }
