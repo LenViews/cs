9560ead7dc514182af4c883279be947e7e025a56
lenox <lenoxviews@gmail.com>
2024-07-06 18:51:32 +0900

    Array basic

---
+++ array/array.c
@@ -0,0 +1,8 @@
+#include  <stdio.h>
+int main ()
+{
+    int num[] = {4, 6, 3, 12, 99, 20};
+    //modify fifth element
+    num[6] = 1;
+    printf("%d", num[6]); 
+}
---
+++ array/array.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main (String [] args){
+        String[] cars = {"Tesla", "Bentley", "Rolls Royce", "Range Rover"};
+        System.out.println(cars[1]);
+    }
+}
