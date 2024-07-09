e0bf5613be8e82ff9a0899955de7ebb095e52e62
lenox <lenoxviews@gmail.com>
2024-07-09 15:48:58 +0900

    Modified method name

--- array/array.c
+++ array/array.c
@@ -3,7 +3,7 @@ int main ()
 {
     int num[] = {4, 6, 3, 12, 99, 20};
     //modify fifth element
-    num[4] = 1;
+    num[4] += 1;
     for(int i=0; i < sizeof(num) / sizeof(num[0]); i++)
     {
         printf("%d", num[i]);
--- array/array.java
+++ array/array.java
@@ -1,4 +1,4 @@
-public class Main{
+public class array{
     public static void main (String [] args){
         String[] cars = {"Tesla", "Bentley", "Rolls Royce", "Range Rover"};
         // System.out.println(cars[1]);
