43ef40c1054fcc895f08254115db0162c56a44f5
lenox <lenoxviews@gmail.com>
2024-07-22 19:44:01 +0900

    Overloading

---
+++ Java/polymorphism/arithmetic.java
@@ -0,0 +1,11 @@
+package polymorphism;
+public class arithmetic {
+    public void sum(int a, int b) {
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    public void sum(int a, int b, int c) {
+        int sum = a+b+c;
+        System.out.println(sum);
+    }
+}
