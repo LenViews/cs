c4f3cc20986c755b1f26c2642df5560cace0cc68
lenox <lenoxviews@gmail.com>
2024-08-06 02:51:51 +0900

    successful connection to database

--- Java/database/src/App.java
+++ Java/database/src/App.java
@@ -5,18 +5,28 @@ import java.sql.Statement;
 
 // import javax.management.Query;
 
-public class App {
+public abstract class App {
+    public abstract void selectOperation();
+    public abstract void updateOperation();
+    public abstract void insertOperation();
+    public abstract void deleteOperation();
+
     public static void main(String[] args) throws Exception {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/Test", "root", "");
+            // System.out.println("Driver loaded successfully!");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from test where id<0");
+            ResultSet rs = stmt.executeQuery("select * from user where id > 0");
             while (rs.next()) {
-                System.out.println();
+                String school = rs.getString("school");
+                String name = rs.getString("name");
+                System.out.println("Name:"+name);
+                System.out.println("School:"+school);
             }
         } catch (ClassNotFoundException cnfe) {
             System.out.println("Could not load driver"+cnfe.getMessage());
         }
+
     }
 }
---
+++ Java/database/src/DBOperation.java
@@ -0,0 +1,18 @@
+import java.sql.PreparedStatement;
+
+public class DBOperation extends App{
+    public void selectOperation(){
+        String query = "select * from user where id = 1";
+        PreparedStatement pst = null;
+        // pst = con.preparedStatement(query);
+    }
+    public void updateOperation(){
+        
+    }
+    public void insertOperation(){
+        
+    }
+    public void deleteOperation(){
+        
+    }
+}
